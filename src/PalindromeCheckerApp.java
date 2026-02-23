d6ed5da67fab390298ab01fc6c23933c52e513bf
Abhi-maan <abhimaanverma@gmail.com>
2026-02-23 15:21:29 +0530

    UC6: Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,71 +1,24 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        //UC1
-        System.out.println("Welcome to the palindrome checker management system");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
 
-        /*UC2: Print a Hardcoded Palindrome Result
-
+        //UC6
         System.out.print("Input text: ");
         String input = scanner.nextLine();
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Is it a Palindrome? : " + isPalindrome);*/
-
-        /*UC3: Palindrome Check Using String Reverse
-
-        System.out.print("Input text: ");
-        String input = scanner.nextLine();
-        String reverse = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reverse = reverse + input.charAt(i);
-        }
-        boolean isPalindrome = input.equals(reverse);
-        System.out.println("Is it a Palindrome? : " + isPalindrome); */
-
-
-        /*UC4: Character Array Based Palindrome Check
-
-        System.out.print("Input text: ");
-        String input = scanner.nextLine();
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);*/
-
-        //UC5
-
-        System.out.print("Input text: ");
-        String input = scanner.nextLine();
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
         boolean isPalindrome = true;
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
